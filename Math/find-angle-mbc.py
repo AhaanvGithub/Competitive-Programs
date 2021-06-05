e0a62c3a8ed5040fc8a56fccfac06b0ef1f080f6
CoffeeliciousGithub <79311038+CoffeeliciousGithub@users.noreply.github.com>
2021-06-05 11:19:17 +0800

    Delete Math directory

--- Math/find-angle-mbc.py
+++
@@ -1,6 +0,0 @@
-# Find Angle MBC
-from math import *
-
-abdist = float(input())
-bcdist = float(input())
-print(str(int(round(degrees(atan(abdist/bcdist)),0))) + "\N{DEGREE SIGN}")
--- Math/polar-coordinates.py
+++
@@ -1,5 +0,0 @@
-# Polar Coordinates
-import cmath
-
-r = complex(input().strip())
-for i in range(2): print(cmath.polar(r)[i])
