b9947ca79068e9c2eb909bee03f2c6a70971ef26
CoffeeliciousGithub <79311038+CoffeeliciousGithub@users.noreply.github.com>
2021-06-08 14:52:29 +0800

    Create piling-up.py

---
+++ Collections/piling-up.py
@@ -0,0 +1,9 @@
+# Piling Up!
+from collections import deque
+def check(d):
+    while d:
+        big = d.popleft() if d[0]>d[-1] else d.pop()
+        if not d: return "Yes"
+        if d[-1] > big or d[0] > big: return "No"
+    
+for i in range(int(input())): int(input()), print(check(deque(map(int,input().split()))))
